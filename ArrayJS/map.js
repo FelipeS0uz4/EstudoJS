8debb4bdc3d409d8a91def13d40a6d84fd50e2ee
FelipeS0uz4 <felipe.panichi@aluno.faculdadeimpacta.com.br>
2025-10-23 17:49:47 -0300

    modified:   ArrayJS/map.js

--- ArrayJS/map.js
+++ ArrayJS/map.js
@@ -5,3 +5,12 @@ let resultado = nums.map(function(e) {
     return e * 2
 })
 console.log(resultado)
+
+//Map não modifica o array original e sim gera um novo array
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.',',')}` //Ira formatar o número para o formato de dinheiro brasileiro
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro)//Foi usado o encadeamento de funções para aplicar várias transformações no array
+console.log(resultado)
